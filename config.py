6dd6eb3e647d47c7591796ed1734ceb2fd7b2716
halbGefressen <bmpepic@gmail.com>
2019-08-19 16:23:26 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+from configparser import ConfigParser
+import os
+
+def create_config():
+    config = ConfigParser()
+    config["Settings"] = {
+        "Token" : "change me",
+        "DefaultChannel": "change me"
+    }
+    if os.path.isfile("./config.cfg"):
+        os.rename("./config.cfg", "./config.cfg.old")
+    with open("./config.cfg", "w") as file:
+        config.write(file)
+
+
+def read_config():
+    parser = ConfigParser()
+    parser.read("config.cfg")
+    return parser["Settings"]["Token"], parser["Settings"]["DefaultChannel"]
+
+if __name__ == __main__: 
+    create_config()
